ec5feee7b4fa4b3e4760afaa3aa4217e4e787ffd
Leonid Ivanushenko <lloonnyyaa@gmail.com>
2021-07-21 17:53:02 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WORKDIR=/home/ec2-user
-export HOST_API=`aws ssm get-parameters --name HOST_API --region eu-central-1 --output text --query Parameters[].Value`
+HOST_API=$(aws ssm get-parameters --name HOST_API --region eu-central-1 --output text --query Parameters[].Value)
 
 cd $WORKDIR
 
